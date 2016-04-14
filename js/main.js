6e73bdda0fb7fe7eefd78c34ff846648777e53d2
Allison Amaral <amaralli@uw.edu>
2016-04-14 12:17:22 -0700

    Setting up traces and layouts

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 $(document).ready(function() {
-	
+	d3.csv('../antibiotics_data.csv', function(data) {
+		console.log(data);
+
+		function processData(rows, key) {
+			return rows.map(function(row) {
+				return row[key];
+			});
+		}
+
+		var trace1 = [{
+
+		}];
+
+		var layout1 = {
+
+		};
+
+		Plotly.newPlot('graph1', trace1, layout1);
+
+		var trace2 = [{
+
+		}];
+
+		var layout2 = {
+
+		};
+
+		Plotly.newPlot('graph2', trace2, layout2);
+
+		var trace3 = [{
+
+		}];
+
+		var layout3 = {
+
+		};
+		
+		Plotly.newPlot('graph3', trace3, layout3);
+
+	});
 });
 
 function buildGraphs() {
-	Plotly.newPlot('graph1', traces1, layout1);
-	Plotly.newPlot('graph2', traces2, layout2);
-	Plotly.newPlot('graph3', traces3, layout3);
+	
 }
